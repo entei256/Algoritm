8227ad15c9255f2831c00dd07d809ac1093d61ac
Игорь <crazydead256@gmail.com>
2022-10-20 11:09:55 +0300

    Done

--- Search/Program.cs
+++ Search/Program.cs
@@ -138,7 +138,19 @@ foreach (var item in SelectedArr)
 }
 #endregion
 
-#region Лгоритмы сортировки
+#region Сортировка Слияния
+Console.WriteLine("\n\nСортировка Слияния");
+Console.WriteLine("До сортировки:");
+foreach (var item in arr)
+{
+    Console.Write(" {0} ", item);
+}
+var MergeArr = MergeSort.Sort(arr);
+Console.WriteLine("\n\nПосле сортировки:");
+foreach (var item in SelectedArr)
+{
+    Console.Write(" {0} ", item);
+}
 #endregion
 
 #region Лгоритмы сортировки
